d08ff77d78981dd4a73fa34c948f1f0ae6bae875
Eugene Cheah <eugeneqin@gmail.com>
2019-10-27 18:56:58 +0800

    group collection

--- src/db/collection/groupCollection.js
+++ src/db/collection/groupCollection.js
@@ -2,10 +2,10 @@
 // Setup the provider collection
 // and return it as a promise object
 //
-const providersCollectionPromise = (async function() {
+const groupCollectionPromise = (async function() {
 	// Get the collection
 	const dbConnector = await require("../mongodb");
-	const collection = await dbConnector.collection("providers");
+	const collection = await dbConnector.collection("groups");
 
 	// Lets setup indexes if needed
 	// collection.createIndex( { "publicURL":1 } );
@@ -13,4 +13,4 @@ const providersCollectionPromise = (async function() {
 	// Alrighto time to return the collection object
 	return collection;
 })();
-module.exports = providersCollectionPromise;
+module.exports = groupCollectionPromise;
