5cc5e30f0059ad54c04bb72a16b561e295326095
Eugene Cheah <eugeneqin@gmail.com>
2019-10-27 10:55:12 +0800

    monogodb client connector

--- src/db/monogodb.js
+++ src/db/monogodb.js
@@ -2,5 +2,26 @@
 // This module setup the respective mongodb connection
 // and returns it in a awaitable promise object
 //
+
+// Load the dependencies
+const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 
+// Load the DB config
+const dbConfig = require( path.resolve( process.cwd(), "config/secure/mongodb" ) )
+
+// Validate DB config
+if( dbConfig.connection == null || dbConfig.connection.length <= 0 ) {
+	throw "Missing `config/secure/mongodb` config file";
+}
+
+// Setup the dbClient
+const dbClientInstance = new MongoClient(dbConfig.connection, { useNewUrlParser: true });
+
+// Client connector wrapped in a promise
+const dbClientConnectorPromise = (async function() {
+	return await dbClientInstance.connect();
+})();
+
+// Return the module exports
+module.exports = dbClientConnectorPromise;
